3dc09a87b88c6b25c28a1005424dd928ff0903fe
charisey <cheima.bessouda@powercoders.org>
2019-03-07 11:11:07 +0100

    mean with js

--- easy/calculate-the-mean/code.js
+++ easy/calculate-the-mean/code.js
@@ -1,5 +1,10 @@
-function mean(arr) {
-  // Your code here.
-}
+function mean(numbers) {
+    let total = 0, i;
+    for (i = 0; i < numbers.length; i += 1) {
+        total += numbers[i];
+    }
+    return Number((total / numbers.length).toFixed(2))
+
+};
 
 module.exports = mean;
